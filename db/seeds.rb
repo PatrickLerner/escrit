8f6314cf0e22de4dbe8b3339bd7799b45aa7dd8a
Patrick Lerner <patricklerner@me.com>
2015-05-20 16:10:27 +0200

    Extended default seed for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,8 @@ Language.create(name: 'Portugese')
 Language.create(name: 'Russian')
 Language.create(name: 'Spanish')
 Language.create(name: 'Other')
+
+Service.create("name"=>"English Wiktionary", "short_name"=>"wikt", "url"=>"http://en.wiktionary.org/wiki/query", "language_id"=>0)
+Service.create("name"=>"Forvo", "short_name"=>"forvo", "url"=>"http://forvo.com/search/query/", "language_id"=>0)
+Service.create("name"=>"Google Translator", "short_name"=>"gtans", "url"=>"https://translate.google.com/#auto/en/query", "language_id"=>0)
+Service.create("name"=>"dict.cc EN<=>DE", "short_name"=>"dict.cc", "url"=>"http://www.dict.cc/?s=query", "language_id"=>4)
---
+++ lib/tasks/export.rake
@@ -0,0 +1,11 @@
+namespace :export do
+  desc "Prints seeds.rb content"
+  task :seeds_format => :environment do
+    Language.order(:id).all.each do |language|
+      puts "Language.create(#{language.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+    end
+    Service.order(:id).all.each do |service|
+      puts "Service.create(#{service.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+    end
+  end
+end
