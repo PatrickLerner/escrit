b9e241f08e93dc533d6964a9a651318231da5752
Patrick Lerner <patricklerner@me.com>
2015-06-07 01:36:32 +0200

    'Number of texts added per week' added as a statistic

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,13 +4,17 @@ class StatisticsController < ApplicationController
   def index
     @new_words_data = []
     @new_words_labels = []
+    @new_texts_data = []
+    @new_texts_labels = []
     14.downto(0).each { |i|
       start_date = (i*7).day.ago.beginning_of_week.beginning_of_day
       end_date = (i*7).day.ago.end_of_week.end_of_day
       if selected_language == nil
         @new_words_data += [Word.where(created_at: start_date..end_date).length]
+        @new_texts_data += [Text.where(created_at: start_date..end_date).length]
       else
         @new_words_data += [Word.where(created_at: start_date..end_date, language_id: selected_language.id).length]
+        @new_texts_data += [Text.where(created_at: start_date..end_date, language_id: selected_language.id).length]
       end
       if i == 0
         @new_words_labels += ["this week"]
@@ -19,6 +23,7 @@ class StatisticsController < ApplicationController
       else
         @new_words_labels += [i.to_s + " weeks ago"]
       end
+      @new_texts_labels = @new_words_labels
     }
   end
 end
