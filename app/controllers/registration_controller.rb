4f72bdfe430fc733ece8953be6abbb159135f7e4
Patrick Lerner <patricklerner@me.com>
2015-07-25 04:42:53 +0200

    Automaticall add services on signup

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationController < Devise::RegistrationsController
+  after_filter :add_account 
+
+  protected
+    def add_account
+      if resource.persisted? # user is created successfuly
+        Service.create!([
+          {name: "dict.cc EN<=>DE", short_name: "dict.cc", url: "http://www.dict.cc/?s={query}", language_id: 4, user_id: resource.id},
+          {name: "Forvo", short_name: "forvo", url: "http://forvo.com/search/{query}/", language_id: 0, user_id: resource.id},
+          {name: "Google Translator", short_name: "gtans", url: "https://translate.google.com/#auto/en/{query}", language_id: 0, user_id: resource.id},
+          {name: "English Wiktionary", short_name: "wikt", url: "http://en.wiktionary.org/wiki/{query}", language_id: 0, user_id: resource.id},
+          {name: "Yandex EN<=>RU", short_name: "yandex", url: "https://translate.yandex.com/?text={query}&lang=ru-en", language_id: 7, user_id: resource.id}
+        ])
+      end
+   end
+end
