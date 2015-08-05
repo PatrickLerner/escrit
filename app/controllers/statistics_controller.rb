a5ca160bdfe1ab797fa8cbdcc65f25b2b6286712
Patrick Lerner <patricklerner@me.com>
2015-08-05 17:23:52 +0200

    Added averages to texts/words in Statistics page

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -7,16 +7,40 @@ class StatisticsController < ApplicationController
     @new_words_labels = []
     @new_texts_data = []
     @new_texts_labels = []
+
+    new_words_sum = 0
+    @new_words_count = 0
+    new_texts_sum = 0
+    @new_texts_count = 0
+
     14.downto(0).each { |i|
       start_date = (i*7).day.ago.beginning_of_week.beginning_of_day
       end_date = (i*7).day.ago.end_of_week.end_of_day
+
+      new_words = 0
+      new_texts = 0
+
       if selected_language == nil
-        @new_words_data += [Word.where(created_at: start_date..end_date, user_id: current_user.id).length]
-        @new_texts_data += [Text.where(created_at: start_date..end_date, user_id: current_user.id).length]
+        new_words = Word.where(created_at: start_date..end_date, user_id: current_user.id).length
+        new_texts = Text.where(created_at: start_date..end_date, user_id: current_user.id).length
       else
-        @new_words_data += [Word.where(created_at: start_date..end_date, language_id: selected_language.id, user_id: current_user.id).length]
-        @new_texts_data += [Text.where(created_at: start_date..end_date, language_id: selected_language.id, user_id: current_user.id).length]
+        new_words = Word.where(created_at: start_date..end_date, language_id: selected_language.id, user_id: current_user.id).length
+        new_texts = Text.where(created_at: start_date..end_date, language_id: selected_language.id, user_id: current_user.id).length
       end
+
+      @new_words_data += [new_words]
+      @new_texts_data += [new_texts]
+
+      if @new_words_count > 0 or new_words > 0
+        @new_words_count += 1
+        new_words_sum += new_words
+      end
+
+      if @new_texts_count > 0 or new_texts > 0
+        @new_texts_count += 1
+        new_texts_sum += new_texts
+      end
+
       if i == 0
         @new_words_labels += ["this week"]
       elsif i == 1
@@ -26,6 +50,11 @@ class StatisticsController < ApplicationController
       end
       @new_texts_labels = @new_words_labels
     }
+    @new_words_average = 0
+    @new_words_average = new_words_sum.to_f / @new_words_count.to_f if @new_words_count > 0
+    @new_texts_average = 0
+    @new_texts_average = new_texts_sum.to_f / @new_texts_count.to_f if @new_texts_count > 0
+
     @words_data = []
     @words_labels = []
     5.times { |i|
