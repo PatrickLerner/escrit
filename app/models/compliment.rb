d3d3dbdd3a6e93b013b4bcfa2d9a7a779a207f80
Patrick Lerner <patricklerner@me.com>
2015-08-07 16:39:21 +0200

    Correct validation for compliments added

--- app/models/compliment.rb
+++ app/models/compliment.rb
@@ -1,4 +1,6 @@
 class Compliment < ActiveRecord::Base
   belongs_to :language
   default_scope { joins(:language).order('languages.name asc, value asc') }
+  validates :value, length: { minimum: 3 }
+  validates :language_id, presence: true
 end
