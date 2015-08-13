283bf7802bbb44905739044dac44480a886917ec
Patrick Lerner <patricklerner@me.com>
2015-08-13 13:43:29 +0200

    Only count non-public texts to see if user has learned a language

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -6,7 +6,7 @@ module TextsHelper
   def language_options filter_unused = false
     options = []
     if filter_unused
-      my_texts = Text.select("texts.*, languages.name as language_name, languages.id as language_id").joins(:language).where(user_id: current_user.id)
+      my_texts = Text.select("texts.*, languages.name as language_name, languages.id as language_id").joins(:language).where(user_id: current_user.id, public: false)
       languages = {}
       my_texts.each { |t|
         languages[t.language_name] = t.language_id
