d35a30630c21386c8812c9eca7eb95d4aa084ea9
Patrick Lerner <patricklerner@me.com>
2015-08-24 19:06:22 +0200

    Added trophy case to user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
     @words = Word.joins(:language).where('user_id = ? and rating < 6', params[:id]).group('languages.name').order('languages.name asc').count
+    @words_familiar = Word.joins(:language).where('user_id = ? and rating < 6 and rating >= 3', params[:id]).group('languages.name').order('languages.name asc').count
   end
 end
