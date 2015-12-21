21d9ffb16c755e2ada36208b586e55aed92949f6
Patrick Lerner <patricklerner@me.com>
2015-12-21 01:39:12 +0100

    Better description of word count on text#index, update word count/normalize migration for existing data added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207162816) do
+ActiveRecord::Schema.define(version: 20151221013616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
