55755b2be7a56f4c6128d9687f4684e0798205cf
Patrick Lerner <patricklerner@me.com>
2016-01-05 21:19:28 +0100

    Updated way language artworks are loaded in

--- app/models/language.rb
+++ app/models/language.rb
@@ -4,8 +4,19 @@ class Language < ActiveRecord::Base
   has_many :texts
   has_many :compliments
   has_many :replacements
+  has_many :language_artworks
 
   def to_param
     self.name.downcase
+  end
+
+  def current_language_artwork
+    self.language_artworks.sample
+  end
+
+  def current_language_artwork_style
+    if self.current_language_artwork
+      "background-image: url(#{self.current_language_artwork.image.url});"
+    end
   end
 end
