2b097bbd0d0fa0c3e83a29e3133705f03b3bb587
Patrick Lerner <patricklerner@me.com>
2016-01-09 01:23:18 +0100

    fix migation

--- app/controllers/vocabularies_controller.rb
+++ app/controllers/vocabularies_controller.rb
@@ -6,7 +6,7 @@ class VocabulariesController < ApplicationController
   end
 
   def index
-    @words = Note.includes(:word).joins(:word).where('notes.user_id = ? AND words.language_id = ? AND vocabulary = TRUE AND ((rating = 0 AND notes.updated_at < ?) OR (rating = 1 AND notes.updated_at < ?) OR (rating = 2 AND notes.updated_at < ?) OR (rating = 3 AND notes.updated_at < ?) OR (rating = 4 AND notes.updated_at < ?) OR ((rating = 5 OR rating = 6) AND notes.updated_at < ?))', current_user.id, current_language.id, 1.days.ago, 2.days.ago, 5.days.ago, 7.days.ago, 13.days.ago, 31.days.ago)
+    @words = Note.includes(:word).joins(:word).where('notes.user_id = ? AND words.language_id = ? AND vocabulary = TRUE AND rating < 6 AND notes.review_at < ?', current_user.id, current_language.id, DateTime.now)
     respond_to do |format|
       format.html
       format.json {
--- db/migrate/20160109000658_add_review_at_to_note.rb
+++ db/migrate/20160109000658_add_review_at_to_note.rb
@@ -5,13 +5,13 @@ class AddReviewAtToNote < ActiveRecord::Migration
     Note.all.each do |note|
       next_review = note.updated_at
 
-      next_review += 1.day   if self.rating == 0
-      next_review += 2.days  if self.rating == 1
-      next_review += 5.days  if self.rating == 2
-      next_review += 7.days  if self.rating == 3
-      next_review += 13.days if self.rating == 4
-      next_review += 31.days if self.rating == 5
-      next_review += 31.days if self.rating == 6
+      next_review += 1.day   if note.rating == 0
+      next_review += 2.days  if note.rating == 1
+      next_review += 5.days  if note.rating == 2
+      next_review += 7.days  if note.rating == 3
+      next_review += 13.days if note.rating == 4
+      next_review += 31.days if note.rating == 5
+      next_review += 31.days if note.rating == 6
           
       self.update(review_at: next_review)
     end
