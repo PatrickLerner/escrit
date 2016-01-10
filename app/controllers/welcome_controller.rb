b9ecedf0414fd43f8afc73a676cb76850dea1d89
Patrick Lerner <patricklerner@me.com>
2016-01-10 15:12:21 +0100

    Fix spelling of initial compliment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,7 +17,7 @@ class WelcomeController < ApplicationController
     if current_user.compliments.size > 0
       @compliment = current_user.compliments.sample
     else
-      @compliment = Compliment.new(value: 'Nice to have you onbaord!', language: Language.find_by(name: 'English'))
+      @compliment = Compliment.new(value: 'Nice to have you on board!', language: Language.find_by(name: 'English'))
     end
   end
 end
