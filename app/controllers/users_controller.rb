675fe265f9ba6842a67034f743f5de13bb57b349
Patrick Lerner <ptlerner@gmail.com>
2016-01-22 23:33:40 +0100

    Refactor user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  before_filter :load_user, only: [ :show, :add, :remove ]
+
   def show
-    @user = User.find_by id: params[:id]
     @words = Note.joins(:word).joins('left join languages on languages.id = words.language_id').where('user_id = ? and rating < 6', params[:id]).group('languages.name').order('languages.name asc').count
     @words_familiar = Note.joins(:word).joins('left join languages on languages.id = words.language_id').where('user_id = ? and rating < 6 and rating >= 3', params[:id]).group('languages.name').order('languages.name asc').count
     buddy = Buddy.find_by origin_id: current_user.id, destination_id: params[:id]
@@ -14,23 +15,27 @@ class UsersController < ApplicationController
   end
 
   def add
-    user = User.find_by id: params[:id]
     buddy = Buddy.find_by origin_id: current_user.id, destination_id: params[:id]
-    if user and not buddy
+    if @user and not buddy
       buddy = Buddy.new
       buddy.origin = current_user
-      buddy.destination = user
+      buddy.destination = @user
       buddy.save
     end
     redirect_to '/u/' + params[:id]
   end
 
   def remove
-    user = User.find_by id: params[:id]
     buddy = Buddy.find_by origin_id: current_user.id, destination_id: params[:id]
-    if user and buddy
+    if @user and buddy
       buddy.destroy
     end
     redirect_to '/u/' + params[:did]
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
   end
 end
