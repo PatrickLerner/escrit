f996228f5d98d03fa211e92580d7b0f84180309e
Patrick Lerner <ptlerner@gmail.com>
2016-02-14 19:36:40 +0100

    extended words spec

--- spec/features/words_spec.rb
+++ spec/features/words_spec.rb
@@ -4,10 +4,11 @@ describe 'words' do
   login_user
 
   let!(:language) { create(:language) }
-  let!(:word) { create(:word, language: language) }
+  let!(:word) { create(:word, language: language, value: 'squid') }
 
   it 'has a word list page' do
-    note = create(:note, user: user, word: word)
+    note = create(:note, user: user, word: word, value: 'test')
+    expect(note.word.language).to eq(language)
     visit edit_word_path(language, note.word.value.downcase)
     visit words_path(language)
     expect(page).to have_content note.word.value
@@ -16,6 +17,7 @@ describe 'words' do
 
   it 'has a word detail page' do
     note = create(:note, user: user, word: word)
+    expect(note.word.language).to eq(language)
     visit edit_word_path(language, note.word.value.downcase)
     expect(page).to have_content note.word.value
     expect(page).to have_content note.value
@@ -39,5 +41,18 @@ describe 'words' do
     expect(page).to have_content note.word.value
     visit words_path(language) + "?q=#{note.value}XXX"
     expect(page).to have_content "No #{language.name} words match your search phrase \"#{note.value}XXX\"."
+  end
+
+  it 'should extract example sentences on the detailed word paged' do
+    text = create(:text, content: 'This is an example sentence in English. This is a second one.', user: user)
+    visit edit_word_path(text.language, 'example')
+    expect(page).to have_content 'example'
+    expect(page).to have_content 'sentence'
+    expect(page).to_not have_content 'second'
+
+    visit edit_word_path(text.language, 'second')
+    expect(page).to_not have_content 'example'
+    expect(page).to_not have_content 'sentence'
+    expect(page).to have_content 'one'
   end
 end
