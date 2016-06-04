059210b1e3b3afaa5785d44f40cae901a6b995e9
Patrick Lerner <ptlerner@gmail.com>
2016-06-04 21:58:12 +0200

    fix crfs token not being set with empty response override

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  after_action :set_csrf_cookie_for_ng
+  before_action :set_csrf_cookie_for_ng
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
