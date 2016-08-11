48eb46f150500e82037242d42a48df10836321cc
Patrick Lerner <ptlerner@gmail.com>
2016-08-12 01:22:01 +0200

    Add confirmation email, change email template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626085342) do
+ActiveRecord::Schema.define(version: 20160811230715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,11 @@ ActiveRecord::Schema.define(version: 20160626085342) do
     t.integer  "native_language_id"
     t.integer  "audio_rate"
     t.string   "role"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["native_language_id"], name: "index_users_on_native_language_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
