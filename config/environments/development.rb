9cd2d321195f2c2666e6c0a5ba0e2e16d11347d8
Patrick Lerner <ptlerner@gmail.com>
2016-08-12 01:23:55 +0200

    Fix email host for tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'escrit.dev' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
 end
