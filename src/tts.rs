b6bafcfc25bcbb54da6a0f60b9b85ba396d66785
Patrick Lerner <ptlerner@gmail.com>
2024-06-19 21:01:36 +0200

    remove unwraps

--- src/tts.rs
+++ src/tts.rs
@@ -12,25 +12,29 @@ use std::{thread, time};
 use tts::*;
 
 pub fn speak(tts: &mut Tts, text: &str) {
-    let voices = tts.voices().unwrap();
-    let voice = voices.iter().find(|voice| voice.name() == "Lesya").unwrap();
-    tts.set_voice(&voice).unwrap();
-
-    tts.speak(text, false).unwrap();
-
-    loop {
-        #[cfg(target_os = "macos")]
-        {
-            let run_loop: id = unsafe { NSRunLoop::currentRunLoop() };
-            unsafe {
-                let date: id = msg_send![class!(NSDate), distantFuture];
-                let _: () = msg_send![run_loop, runMode:NSDefaultRunLoopMode beforeDate:date];
-            }
+    if let Ok(voices) = tts.voices() {
+        let voice = voices.iter().find(|voice| voice.name() == "Lesya");
+        if let Some(voice) = voice {
+            let _ = tts.set_voice(voice);
         }
-        let time = time::Duration::from_millis(100);
-        thread::sleep(time);
-        if !tts.is_speaking().unwrap() {
-            break;
+    }
+
+    if tts.speak(text, false).is_ok() {
+        loop {
+            #[cfg(target_os = "macos")]
+            {
+                let run_loop: id = unsafe { NSRunLoop::currentRunLoop() };
+                unsafe {
+                    let date: id = msg_send![class!(NSDate), distantFuture];
+                    let _: () = msg_send![run_loop, runMode:NSDefaultRunLoopMode beforeDate:date];
+                }
+            }
+            let time = time::Duration::from_millis(100);
+            thread::sleep(time);
+
+            if !tts.is_speaking().unwrap_or(false) {
+                break;
+            }
         }
     }
 }
